5bd0205cbf60254777bad0e45a870217f4ec8f45
Deya BERGER <dberger@e1r11p4.42.fr>
2021-05-27 16:25:32 +0200

    verbose

--- classes/logisticregression.py
+++ classes/logisticregression.py
@@ -24,31 +24,26 @@ class ModelEvaluation:
 		self.true_negative = np.zeros((4, 1))
 		self.false_negative = np.zeros((4, 1))
 		self.false_positive = np.zeros((4, 1))
-		self.precision_total, self.sensitivity_total, self.specificity_total, self.accuracy_total, self.F1_score_total = [], [], [], [], []
-		self.iterations = []
-	
+		self.precision_total, self.sensitivity_total, self.accuracy_total, self.F1_score_total = [], [], [], []
+
 	def calculate_kpi(self):
 		''' Commonly use KPIs:
 		Precision: expresses the proportion of the data points our model says was relevant actually were relevant.
 		Sensitivity: (also called redcall) expresses the ability to find all relevant instances in a dataset.
-		Specificity: refers to how well our model identifies our students to not be part of a certain house.
 		Accuracy: proportion of correct predictions over total predictions.
 		F1 Score: the harmonic mean of the model s precision and recall.
 		'''
 		self.precision = self.my_divide(self.true_positive, (self.true_positive + self.false_positive))
 		self.sensitivity = self.my_divide(self.true_positive, (self.true_positive + self.false_negative))
-		self.specificity = self.my_divide(self.true_negative, (self.true_negative + self.false_positive))
 		self.accuracy = self.my_divide((self.true_positive + self.true_negative), (self.true_positive + self.true_negative + self.false_positive + self.false_negative))
 		self.F1_score = (2 * (self.my_divide((self.precision * self.sensitivity), (self.precision + self.sensitivity))))
 	
-	def save_evolution(self, epochs):
+	def save_evolution(self):
 		''' Saving all values of our kpis during each call to the evaluate function, to display them in the graphs '''
-		self.precision_total.append(np.mean(self.precision) * 100)
-		self.sensitivity_total.append(np.mean(self.sensitivity) * 100)
-		self.specificity_total.append(np.mean(self.specificity) * 100)
-		self.accuracy_total.append(np.mean(self.accuracy) * 100)
-		self.F1_score_total.append(np.mean(self.F1_score) * 100)
-		self.iterations.append(epochs)
+		self.precision_total.append(round(np.mean(self.precision), 3))
+		self.sensitivity_total.append(round(np.mean(self.sensitivity), 3))
+		self.accuracy_total.append(round(np.mean(self.accuracy), 3))
+		self.F1_score_total.append(round(np.mean(self.F1_score), 3))
 
 	def evaluate(self, model, epochs, X, y):
 		'''
@@ -66,28 +61,29 @@ class ModelEvaluation:
 				self.false_positive[predicted_house] += 1
 				self.false_negative[real_house] += 1
 		self.calculate_kpi()
-		self.save_evolution(epochs)
+		self.save_evolution()
 
 	def my_divide(self, a, b):
 		result = np.divide(a, b, out=np.zeros_like(a), where=b!=0)
 		return(result)
 
-	def show_graph(self, name, x, y):
+	def show_graph(self, name, x, y, fig):
+		plt.figure(fig)
 		plt.title(f"{name} evolution")
 		plt.xlabel("iterations")
 		plt.ylabel(name)
 		plt.plot(x, y)
-		plt.show()
+		plt.pause(0.001)
+
 
 class LogisticRegression:
 	
 	def __init__(self, args = None, train = True):
 		self.args = args
 		self.read_csv(self.args.datafile, train)
-		if train == True and self.args.activation == "softmax":
+		self.activation = self.sigmoid
+		if self.args.activation == "softmax":
 			self.activation = self.softmax
-		else:
-			self.activation = self.sigmoid
 	
 	def check_input(self, df, features, train):
 		if features == []:
@@ -104,9 +100,9 @@ class LogisticRegression:
 
 	def read_csv(self, datafile, train = True):
 		''' Reading our Training data
-		Features: only numeric values (not taking in account the names of the students for example)
-		X will be our matrix of students and their grades the different subjects
-		y will be the houses of these students
+		Features: only taking in account columns that have numeric values (starting from column 6)
+		X = matrix of (students, grades_for_each_feature)
+		y = matrix of (students, houses). One hot encoding: changing a house name into a vector like [0,0,1,0]
 		For each missing values in grades, we replace it by the median grade of all students
 		'''
 		try:
@@ -121,7 +117,7 @@ class LogisticRegression:
 			else:
 				self.features = self.args.features
 			if self.args.verbose == 1:
-				print(f'-list of features used for logical regression :\n{self.features}\n')
+				print(f'- Features used for our Logistic Regression training :\n{self.features}\n')
 			self.X = df[self.features].to_numpy()
 			one_hot_encoding = pd.get_dummies(df["Hogwarts House"], drop_first = False)
 			self.houses = list(one_hot_encoding.columns)
@@ -157,7 +153,7 @@ class LogisticRegression:
 		''' Splitting our data in a training set and a testing set, so when we calculate our score, we avoid bias and overfitting
 		'''
 		if self.args.verbose == 1:
-			print(f"- Splitting our data into a training and a testing set _ training set = [{round(self.args.train_size * 100)}%], test_set = [{round((1 - self.args.train_size) * 100)}] -\n")
+			print(f"- Splitting our data into a training and a testing set _ training set = [{round(self.args.train_size * 100)}%], test_set = [{round((1 - self.args.train_size) * 100)}%] -\n")
 		self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, train_size=self.args.train_size, random_state=42)
 	
 	def init_weights(self):
@@ -217,42 +213,44 @@ class LogisticRegression:
 		predicted_house = np.argmax(self.H[i])
 		real_house = np.argmax(y[i])
 		return (predicted_house, real_house)
+
+	
+	def calculate_cost(self, X, y):
+		ylogh = y * np.log(self.H)
+		losses = np.sum(ylogh, axis = 1, keepdims = True)
+		self.cost = -1.0 * (np.mean(losses))
+		self.total_cost.append(self.cost)
+
 	
 	def fit(self, score):
 		'''
 		Calculate our predictions, then compute loss gradient according to our error
 		'''
+		self.total_cost = []
 		if self.args.verbose == 1:
 			print(f"- Fitting our model to minimize our cost and find the best values for out thetas: -")
 			print(f"nb of iterations = [{self.args.epochs}]\nactivation function = [{self.args.activation}]\nstochastic gradient descent = [{self.args.stochastic}]\n")
-		F1_convergence, acc_convergence = 0, 0
 		for i in range(self.args.epochs):
 			score.evaluate(self, i, self.X_test, self.y_test)
 			X, y = self.X_train, self.y_train
-			if i == 0 and self.args.verbose == 1:
-				print(f"--> Before training:\naverage F1_score = {round(np.mean(score.F1_score) * 100)}\naverage accuracy = {round(np.mean(score.accuracy) * 100)}\n")
-			if F1_convergence == 0 and np.mean(score.F1_score) >= 0.98:
-				F1_convergence = i
-			if acc_convergence == 0 and np.mean(score.accuracy) >= 0.98:
-				acc_convergence = i
 			if self.args.stochastic == True:
 				X, y = self.choose_stochastic_batch(X, y)
 			self.hypothesis(X)
+			self.calculate_cost(X, y)
 			self.compute_loss_gradient(X, y)
 			self.gradient_descent()
+			if i == 0 and self.args.verbose == 1:
+				print(f"--> Before training:\nCost = {round(self.cost, 3)}\n*average F1_score = {round(np.mean(score.F1_score), 3)}\n*average accuracy = {round(np.mean(score.accuracy), 3)}\n")
+
 		if self.args.verbose == 1:
-			print(f"--> After training:\naverage F1_score = {round(np.mean(score.F1_score) * 100)}\naverage accuracy = {round(np.mean(score.accuracy) * 100)}\n")
-			if F1_convergence != 0:
-				print(f"average F1 score superior to 0.98 after {F1_convergence} epochs\n")
-			if acc_convergence != 0:
-				print(f"average accuracy superior to 0.98 after {acc_convergence} epochs\n")
+			print(f"--> After training:\nCost = {round(self.cost, 3)}\n*average F1_score = {round(np.mean(score.F1_score), 3)}\n*average accuracy = {round(np.mean(score.accuracy), 3)}\n")
 
 			
 	
 	def save_weights(self, file_name):
 		if self.args.verbose == 1:
-			print(f"- Saving our weights, scaling info and houses name into a file cald {file_name} -\n")
-		info = {"thetas" : self.thetas, "scaler" : self.scaler, "houses" : self.houses, "features" : self.features}
+			print(f"- Saving our weights, scaling info and houses name into a file called {file_name} -\n")
+		info = {"thetas" : self.thetas, "houses" : self.houses, "features" : self.features}
 		with open(file_name, "wb") as f:
 			pickle.dump(info, f)
 
@@ -268,18 +266,16 @@ class LogisticRegression:
 			predictions.append(house)
 		self.create_df_predictions(predictions, predictions_file)
 	
-	def display_coefficients(self, coeff_file):
-		with open(coeff_file, 'rb') as f:
-			info = pickle.load(f)
+	def display_weights(self):
 		x = np.arange(len(self.features))
 		width = 0.35
 		fig, ax = plt.subplots()
-		ax.bar([i for i in range(len(info['thetas']) - 1)], [x[0] for x in info['thetas'][1:]], width, label=info['houses'][0])
-		ax.bar([i for i in range(len(info['thetas']) - 1)], [x[1] for x in info['thetas'][1:]], width, label=info['houses'][1])
-		ax.bar([i for i in range(len(info['thetas']) - 1)], [x[2] for x in info['thetas'][1:]], width, label=info['houses'][2])
-		ax.bar([i for i in range(len(info['thetas']) - 1)], [x[3] for x in info['thetas'][1:]], width, label=info['houses'][3])
+		ax.bar([i for i in range(len(self.thetas) - 1)], [x[0] for x in self.thetas[1:]], width, label=self.houses[0])
+		ax.bar([i for i in range(len(self.thetas) - 1)], [x[1] for x in self.thetas[1:]], width, label=self.houses[1])
+		ax.bar([i for i in range(len(self.thetas) - 1)], [x[2] for x in self.thetas[1:]], width, label=self.houses[2])
+		ax.bar([i for i in range(len(self.thetas) - 1)], [x[3] for x in self.thetas[1:]], width, label=self.houses[3])
 		ax.set_xticks(x)
 		ax.set_xticklabels(self.features, rotation='vertical', fontdict={'fontsize': 8})
+		ax.set_title('Weights (thetas) for each features')
 		ax.legend()
 		fig.tight_layout()
-		plt.show()
